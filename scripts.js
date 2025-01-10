3c0fc039552213a79fdec6db52604d01f258cdf8
Khalis189 <133336938+Khalis189@users.noreply.github.com>
2025-01-10 17:36:53 +0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
 document.addEventListener('scroll', function() {
     const section = document.querySelector('.section');
     const place = document.querySelector('.place');
+    const dresscode = document.querySelector('.dresscode');
     const sectionPosition = section.getBoundingClientRect().top;
     const placePosition = place.getBoundingClientRect().top;
+    const dresscodePosition = dresscode.getBoundingClientRect().top;
     const placeHeight = place.getBoundingClientRect().height;
+    const dresscodeHeight = dresscode.getBoundingClientRect().height;
     const screenPosition = window.innerHeight / 1.3;
     const flowers = document.querySelectorAll('.flower');
     const header = document.querySelector('.header');
     const placeFlowers = place.querySelectorAll('.flower.place-left-bottom, .flower.place-right-bottom');
+    const dresscodeFlowers = dresscode.querySelectorAll('.flower.dresscode-left-bottom, .flower.dresscode-right-bottom');
 
     if (sectionPosition < screenPosition) {
         section.classList.add('visible');
@@ -35,5 +39,19 @@ document.addEventListener('scroll', function() {
             flower.style.opacity = '0';
         });
         place.style.opacity = '0';
+    }
+
+    if (dresscodePosition < window.innerHeight / 2 && dresscodePosition > -dresscodeHeight / 2) {
+        dresscodeFlowers.forEach(flower => {
+            flower.style.transition = 'opacity 1s';
+            flower.style.opacity = '1';
+        });
+        dresscode.style.opacity = '1';
+    } else {
+        dresscodeFlowers.forEach(flower => {
+            flower.style.transition = 'opacity 1s';
+            flower.style.opacity = '0';
+        });
+        dresscode.style.opacity = '0';
     }
 });

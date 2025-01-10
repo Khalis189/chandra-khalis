471d1c237c6022a8522e167f85b998185413a640
Khalis189 <133336938+Khalis189@users.noreply.github.com>
2025-01-10 19:24:42 +0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,16 +2,20 @@ document.addEventListener('scroll', function() {
     const section = document.querySelector('.section');
     const place = document.querySelector('.place');
     const dresscode = document.querySelector('.dresscode');
+    const itinerary = document.querySelector('.itinerary');
     const sectionPosition = section.getBoundingClientRect().top;
     const placePosition = place.getBoundingClientRect().top;
     const dresscodePosition = dresscode.getBoundingClientRect().top;
+    const itineraryPosition = itinerary.getBoundingClientRect().top;
     const placeHeight = place.getBoundingClientRect().height;
     const dresscodeHeight = dresscode.getBoundingClientRect().height;
+    const itineraryHeight = itinerary.getBoundingClientRect().height;
     const screenPosition = window.innerHeight / 1.3;
     const flowers = document.querySelectorAll('.flower');
     const header = document.querySelector('.header');
     const placeFlowers = place.querySelectorAll('.flower.place-left-bottom, .flower.place-right-bottom');
     const dresscodeFlowers = dresscode.querySelectorAll('.flower.dresscode-left-bottom, .flower.dresscode-right-bottom');
+    const itineraryFlowers = itinerary.querySelectorAll('.flower.itinerary-left-bottom, .flower.itinerary-right-bottom, .flower.itinerary-right-top');
 
     if (sectionPosition < screenPosition) {
         section.classList.add('visible');
@@ -53,5 +57,19 @@ document.addEventListener('scroll', function() {
             flower.style.opacity = '0';
         });
         dresscode.style.opacity = '0';
+    }
+
+    if (itineraryPosition < window.innerHeight / 2 && itineraryPosition > -itineraryHeight / 2) {
+        itineraryFlowers.forEach(flower => {
+            flower.style.transition = 'opacity 1s';
+            flower.style.opacity = '1';
+        });
+        itinerary.style.opacity = '1';
+    } else {
+        itineraryFlowers.forEach(flower => {
+            flower.style.transition = 'opacity 1s';
+            flower.style.opacity = '0';
+        });
+        itinerary.style.opacity = '0';
     }
 });

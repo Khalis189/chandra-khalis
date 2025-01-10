e7173cafc347da409f03ca81a5d77d7318a8174d
Khalis189 <133336938+Khalis189@users.noreply.github.com>
2025-01-10 11:24:39 +0700

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
 document.addEventListener('scroll', function() {
     const section = document.querySelector('.section');
+    const place = document.querySelector('.place');
     const sectionPosition = section.getBoundingClientRect().top;
+    const placePosition = place.getBoundingClientRect().top;
+    const placeHeight = place.getBoundingClientRect().height;
     const screenPosition = window.innerHeight / 1.3;
     const flowers = document.querySelectorAll('.flower');
     const header = document.querySelector('.header');
+    const placeFlowers = place.querySelectorAll('.flower.place-left-bottom, .flower.place-right-bottom');
 
     if (sectionPosition < screenPosition) {
         section.classList.add('visible');
@@ -17,5 +21,19 @@ document.addEventListener('scroll', function() {
             flower.style.opacity = '1';
         });
         header.style.opacity = '1';
+    }
+
+    if (placePosition < window.innerHeight / 2 && placePosition > -placeHeight / 2) {
+        placeFlowers.forEach(flower => {
+            flower.style.transition = 'opacity 1s';
+            flower.style.opacity = '1';
+        });
+        place.style.opacity = '1';
+    } else {
+        placeFlowers.forEach(flower => {
+            flower.style.transition = 'opacity 1s';
+            flower.style.opacity = '0';
+        });
+        place.style.opacity = '0';
     }
 });
